a30d948ad371e5ba15960ff070c40054b0f4fe34
Jozef <jozefw@sbcglobal.net>
2017-09-25 14:22:51 -0700

    calback simutlation

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-let promiseResult = new Promise((resolve, reject) => {
-    resolve();
+promiseResult = new Promise((resolve, reject) => {
+    setTimeout(() => {
+    	resolve();
+    },3000)
 });
 
-promiseResult.then(() => {
+promiseResult
+.then(() => {
   console.log('We are finshed here!');
-});
+})
+.then(() => {
+	console.log("I also finshed with this!")
+})
+.catch(() => {
+	console.log("Uh, oh...I reject this.")
+})
 promiseResult.catch(() => {
 
 });
